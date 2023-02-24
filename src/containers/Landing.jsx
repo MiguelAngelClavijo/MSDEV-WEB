33e8a47a45926830385ad8e2571e1644b2230c8b
Miguel Clavijo <clavijomiguel20@gmail.com>
2023-02-24 00:48:02 -0500

    [ADD] Background icons landing page

--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/Landing.css";
+import keysicon from "../assets/icons/keysicon.svg";
+import reacticon from "../assets/icons/reacticon.svg";
+import jsicon from "../assets/icons/jsicon.svg";
+import cssicon from "../assets/icons/cssicon.svg";
 
 const Landing = () => {
   return (
     <div className="Landing-Container">
       <div className="Landing-Container-Left">
+        <img src={keysicon} />
+        <img src={reacticon} />
         <p>Tu sitio web es un reflejo de ti y de tu marca</p>
         <p>
           Permítenos ayudarte a causar una buena impresión con un diseño único
@@ -16,7 +22,10 @@ const Landing = () => {
         </div>
       </div>
       <div className="Landing-Container-Right">
+        <img src={jsicon} />
+        <img src={cssicon} />
         <div className="Black-Card"></div>
+        <div className="Green-Card"></div>
       </div>
     </div>
   );
