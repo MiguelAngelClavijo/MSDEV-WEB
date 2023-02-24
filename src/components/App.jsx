40772007f4c5ce16e8ade0de60bc5dc36e74ebaf
unknown <Sergio Gomez>
2023-02-24 09:08:10 -0500

    [ADD] stepcard and stepbystep layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Stepbystep } from "../containers/Stepbystep";
 import "../styles/global.scss";
 import { Navbar } from "./navbar";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Stepbystep/>
     </div>
   );
 };
