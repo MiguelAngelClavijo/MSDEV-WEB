83710ba6e021ba9692dad2763c3b42a8e4edf9db
Miguel Clavijo <clavijomiguel20@gmail.com>
2023-03-06 14:54:58 -0500

    [ADD] Copy call to action

--- src/containers/Calltoaction.jsx
+++ src/containers/Calltoaction.jsx
@@ -17,7 +17,8 @@ const Calltoaction = () => {
         negocio con un diseño ilimitado.
       </p>
       <button onClick={link} className="Calltoaction-Button-Cta">
-        Eres libre de escoger, Comienza ya →
+        Eres libre de escoger, Agenda{" "}
+        <span id="Calltoaction-Button-Cta-Free">Gratis</span> tu cita ahora →
       </button>
     </div>
   );
