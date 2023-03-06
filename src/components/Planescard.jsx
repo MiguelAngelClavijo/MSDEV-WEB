89635ccc47b3caef50584228f63f59ede6eeaac1
Miguel Clavijo <clavijomiguel20@gmail.com>
2023-03-05 22:22:13 -0500

    [ADD|FIX] Price Cards, Styles fix, merge branchs

--- src/components/Planescard.jsx
+++ src/components/Planescard.jsx
@@ -1,28 +1,23 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "../styles/Planescard.css";
-import { motion, useMotionValue, useTransform, animate} from "framer-motion";
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 
 const Planescard = (props) => {
-
-  
-  const count = useMotionValue(props.Planescard_number)
+  const count = useMotionValue(props.Planescard_number);
   const rounded = useTransform(count, Math.round);
 
   useEffect(() => {
-    
-    const animation = animate(count, props.Planescard_target, { duration: 1});
-    console.log(props.Planescard_target)
+    const animation = animate(count, props.Planescard_target, { duration: 1 });
+    console.log(props.Planescard_target);
     return animation.stop;
-
-    
   }, [props.Planescard_target]);
   const link = () => {
-    var message = "https://wa.me/573178225494?text="+props.Planescard_message
+    var message = "https://wa.me/573178225494?text=" + props.Planescard_message;
     window.open(message);
   };
 
   var Benefits = props.Planescard_text.map(function (i, index) {
-    return <li key = {index}>✓ {i}.</li>;
+    return <li key={index}>✓ {i}.</li>;
   });
 
   return (
@@ -33,13 +28,14 @@ const Planescard = (props) => {
       <section className="Planescard-price-container">
         <p> Desde:</p>
         <div>
-        <span>$</span>
-        <motion.span>{rounded}</motion.span>
+          <div className="Planescard-price-container-text">
+            <motion.span>{rounded}</motion.span>
+            <span>K</span>
+            <span id="Planescard-price-container-text-cop"> cop</span>
+          </div>
+          <button onClick={link}>{props.Planescard_button}</button>
         </div>
       </section>
-      <div>
-        <button onClick={link}>{props.Planescard_button}</button>
-      </div>
     </div>
   );
 };
--- src/containers/Planes.jsx
+++ src/containers/Planes.jsx
@@ -4,11 +4,10 @@ import dolarSign from "../assets/icons/dolarSign.svg";
 import { Planescard } from "../components/Planescard";
 import "../styles/Planes.css";
 
-
 const Planescard_Basic = [
   "Garantizamos un resultado exactamente igual a la propuesta",
   "Conexión con una red social para aumentar tu alcance",
-  "Diseño de Página web Estática con Call to Action"
+  "Diseño de Página web Estática con Call to Action",
 ];
 
 const Planescard_Deluxe = [
@@ -17,7 +16,6 @@ const Planescard_Deluxe = [
   "Funcionalidades de correo y redes sociales",
   "Asesoramiento de desarrollo y de proyecto",
   "Sitio Web dinámico y llamativo",
-
 ];
 const Planescard_Master = [
   "Posicionamiento inicial en linea para tu marca o proyecto",
@@ -25,42 +23,44 @@ const Planescard_Master = [
   "Integración completa con tu estrategia de marketing",
   "Experiencia de usuario Excepcional",
   "Soporte Técnico prioritario",
-
 ];
 
-const basictextWith = "¡Hola! Sabemos que cuentas con un diseño propio. Estás un paso más cerca de llevar tu proyecto al siguiente nivel. Con este plan Básico, tendrás una presencia online a un precio accesible. %0A ¡Comencemos! %0A ¡Agendemos una cita lo antes posible!"
-const basictextWithout = "¡Genial! Has elegido el plan básico para dar el primer paso en tu proyecto, déjanos ayudarte creando desde cero el diseño de tu empresa. Con esta opción, tendrás lo que necesitas para %0A empezar a destacar en línea. ¡Adelante! %0A ¡Agendemos una cita lo antes posible!"
-const deluxeTextWith = "¡Excelente elección! Implementemos tu imagen de marca. Con el plan Deluxe, podrás personalizar y optimizar tu presencia en línea para un mayor impacto en tus clientes. ¡Estamos emocionados de trabajar juntos!%0A¡Agendemos una cita lo antes posible!"
-const deluxeTextWithout = "¡Vamos por más! Con el plan Deluxe, obtendrás una presencia en línea de alta calidad y personalizada creando juntos desde 0 la imagen de tu empresa, para hacer crecer tu proyecto al siguiente nivel. ¡Listos para comenzar!%0A¡Agendemos una cita lo antes posible!"
-const masterTextWith = "¡Es hora de despegar! Con el plan Máster, tendrás todo lo que necesitas para llevar tu proyecto al siguiente nivel, incluyendo el diseño de tu empresa, funcionalidades y una presencia online personalizada. ¡Estamos listos!%0A¡Agendemos una cita lo antes posible!"
-const masterTextWithout = "¡Felicidades! Has elegido el mejor plan para hacer realidad tus objetivos. Con nuestra solución personalizada, crearemos a la medida la imagen de tu empresa así estarás en camino a alcanzar el éxito que mereces.%0A¡Agendemos una cita lo antes posible"
-
-
+const basictextWith =
+  "¡Hola! Sabemos que cuentas con un diseño propio. Estás un paso más cerca de llevar tu proyecto al siguiente nivel. Con este plan Básico, tendrás una presencia online a un precio accesible. %0A ¡Comencemos! %0A ¡Agendemos una cita lo antes posible!";
+const basictextWithout =
+  "¡Genial! Has elegido el plan básico para dar el primer paso en tu proyecto, déjanos ayudarte creando desde cero el diseño de tu empresa. Con esta opción, tendrás lo que necesitas para %0A empezar a destacar en línea. ¡Adelante! %0A ¡Agendemos una cita lo antes posible!";
+const deluxeTextWith =
+  "¡Excelente elección! Implementemos tu imagen de marca. Con el plan Deluxe, podrás personalizar y optimizar tu presencia en línea para un mayor impacto en tus clientes. ¡Estamos emocionados de trabajar juntos!%0A¡Agendemos una cita lo antes posible!";
+const deluxeTextWithout =
+  "¡Vamos por más! Con el plan Deluxe, obtendrás una presencia en línea de alta calidad y personalizada creando juntos desde 0 la imagen de tu empresa, para hacer crecer tu proyecto al siguiente nivel. ¡Listos para comenzar!%0A¡Agendemos una cita lo antes posible!";
+const masterTextWith =
+  "¡Es hora de despegar! Con el plan Máster, tendrás todo lo que necesitas para llevar tu proyecto al siguiente nivel, incluyendo el diseño de tu empresa, funcionalidades y una presencia online personalizada. ¡Estamos listos!%0A¡Agendemos una cita lo antes posible!";
+const masterTextWithout =
+  "¡Felicidades! Has elegido el mejor plan para hacer realidad tus objetivos. Con nuestra solución personalizada, crearemos a la medida la imagen de tu empresa así estarás en camino a alcanzar el éxito que mereces.%0A¡Agendemos una cita lo antes posible";
 
 const Planes = () => {
-  const [number1, setNumber1] = useState(30)
-  const [number2, setNumber2] = useState(60)
-  const [number3, setNumber3] = useState(90)
-  const [masterText, setMasterText] = useState("master")
-  const [deluxeText, setDeluxeText] = useState("deluxe")
-  const [basicText, setbasicText] = useState("basic")
+  const [number1, setNumber1] = useState(30);
+  const [number2, setNumber2] = useState(60);
+  const [number3, setNumber3] = useState(90);
+  const [masterText, setMasterText] = useState("master");
+  const [deluxeText, setDeluxeText] = useState("deluxe");
+  const [basicText, setbasicText] = useState("basic");
   const [isActive, setIsActive] = useState(false);
   const toggle = () => {
-    setIsActive(!isActive)
-  }
+    setIsActive(!isActive);
+  };
 
   useEffect(() => {
-    isActive ? setbasicText(basictextWith) : setbasicText(basictextWithout)
-    isActive ? setDeluxeText(deluxeTextWith) : setDeluxeText(deluxeTextWithout)
-    isActive ? setMasterText(masterTextWith) : setMasterText(masterTextWithout)
-    isActive ? setNumber1(10) : setNumber1(30)
-    isActive ? setNumber2(30) : setNumber2(50)
-    isActive ? setNumber3(60) : setNumber3(80)
-  })
+    isActive ? setbasicText(basictextWith) : setbasicText(basictextWithout);
+    isActive ? setDeluxeText(deluxeTextWith) : setDeluxeText(deluxeTextWithout);
+    isActive ? setMasterText(masterTextWith) : setMasterText(masterTextWithout);
+    isActive ? setNumber1(100) : setNumber1(150);
+    isActive ? setNumber2(300) : setNumber2(350);
+    isActive ? setNumber3(700) : setNumber3(800);
+  });
 
   return (
     <div className="Planes-container" id="Planes">
-
       <div>
         <img src={dolarSign} />
         <p>Nuestros planes</p>
@@ -75,11 +75,11 @@ const Planes = () => {
             <motion.div
               className="Planes-handle"
               data-ison={isActive}
-              animate={{ transition: { type: "spring", stiffness: 700, damping: 30 } }}
+              animate={{
+                transition: { type: "spring", stiffness: 700, damping: 30 },
+              }}
               layout
-            >
-
-            </motion.div>
+            ></motion.div>
           </div>
           <span>Si</span>
         </div>
@@ -116,7 +116,10 @@ const Planes = () => {
           Planescard_target={number3}
         />
       </div>
-      <p>*Todos nuestros planes cuentan con vista adaptada a todos los dispositivos. </p>
+      <p>
+        *Todos nuestros planes cuentan con vista adaptada a todos los
+        dispositivos.{" "}
+      </p>
       <p>*Los planes que requieran diseño incluyen 2 revisiones. </p>
     </div>
   );
